1a3136904170b5fc7f865f64921b9f18de8d1ee5
unknown <00033721@uca.edu.sv>
2023-04-12 16:46:51 -0600

    :lipstick: add styled elements for view component

--- chinawok/src/Components/Main/SeParteContainer/SeParteContainer.component.jsx
+++ chinawok/src/Components/Main/SeParteContainer/SeParteContainer.component.jsx
@@ -12,8 +12,8 @@ const SeParteContainer = ()=>{
         <section className = {` bg-[url(./imgs/mainSection.jpg)] h-[630px] sm:h-[750px] w-full bg-center bg-no-repeat bg-cover flex flex-col items-center`}>
             <img src={experienceImg} alt="Personal con experiencia"  className={` w-[80%] md:w-[50%] pt-[10%] sm:pt-[10%] mb-8 md:mb-24`}/>
             <div className=" flex flex-col md:flex-row w-[70%] md:w-[80%]  justify-around">
-                <Button><Link to={"/registro"} className=" text-white text-lg sm:text-xl md:text-2xl uppercase">aplica por primera vez</Link></Button>
-                <Button><Link to={"/actualiza-perfil"} className=" text-white text-lg sm:text-xl md:text-2xl uppercase">actualiza tu perfil</Link></Button>
+                <Button className="rounded-2xl m-3"><Link to={"/registro"} className=" text-white text-lg sm:text-xl md:text-2xl uppercase">aplica por primera vez</Link></Button>
+                <Button className="rounded-2xl m-3"><Link to={"/actualiza-perfil"} className=" text-white text-lg sm:text-xl md:text-2xl uppercase">actualiza tu perfil</Link></Button>
             </div>
         </section>
     )
