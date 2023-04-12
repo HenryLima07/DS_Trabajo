4d28a2d39eee10ebe454d06d79174cf4197f93da
unknown <00033721@uca.edu.sv>
2023-04-12 17:00:23 -0600

    :lipstick: add styled ContactUs view

--- chinawok/src/Views/ContactUs.view.jsx
+++ chinawok/src/Views/ContactUs.view.jsx
@@ -1,10 +1,14 @@
 import Header from "../Components/Shared/Header/Header.component";
 import Footer from "../Components/Shared/Footer/Footer.component";
+import ContactUsContainer from "../Components/ContactUS/ContactUs.component";
+import Title from "../Components/Shared/Title/TitleContainer.component";
 
 const ContactUs  = ()=>{
     return(
         <section>
             <Header />
+            <Title Title="¿Deseas ponerte en contacto con nosotros?" subTitle="Puedes escribirnos al siguiente correo:"/>
+            <ContactUsContainer />
             <Footer />
         </section>
     )
