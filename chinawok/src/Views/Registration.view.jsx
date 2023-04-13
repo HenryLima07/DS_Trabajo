1feb3e9212c72915e5ca339f788b73b0cee9e6a1
unknown <00033721@uca.edu.sv>
2023-04-13 11:12:56 -0600

    :lipstick: add registration views

--- chinawok/src/Views/Registration.view.jsx
+++ chinawok/src/Views/Registration.view.jsx
@@ -1,10 +1,24 @@
 import Header from "../Components/Shared/Header/Header.component";
 import Footer from "../Components/Shared/Footer/Footer.component";
+import Title from "../Components/Shared/Title/TitleContainer.component";
+import FirstRegistrationContainer from "../Components/Registration/FirstRegistration/FirstRegistration.component";  
+
+//modules
+import { getItemLS } from "../Components/Registration/Registration.module";
+
+//getting data from local storage
+const localStorageData = getItemLS();
+const DBData = false;
+
 
 const Registration = ()=>{
     return(
         <section>
             <Header />
+            <Title Title="¡Tenés todo el talento que andamos buscando!" />
+            <FirstRegistrationContainer 
+                 data={DBData ? DBData : []} 
+                dataFrom={DBData ? "database" : "localstorage"} />
             <Footer />
         </section>
     );
