0e25a6699fa911bd37f3e6f86c25c9bde62ded7c
unknown <00033721@uca.edu.sv>
2023-04-21 10:15:10 -0600

    :bug: fix input validation

--- pizzahut/src/Components/Shared/Title/TitleContainer.component.jsx
+++ pizzahut/src/Components/Shared/Title/TitleContainer.component.jsx
@@ -1,11 +1,11 @@
 
 const JobsContainer = ({Title="", subTitle=""})=>{
     return(
-       <section className="flex flex-col w-full bg-pizzaRed">
+       <section className="flex flex-col w-full bg-pizzaRed font-lato">
             <h1 className={`text-4xl text-white pt-4 text-center w-full ${subTitle ? "": "py-8"}`}>{Title}</h1>
             {
                 subTitle ? 
-                    <h3 className={`font-wendysSimpleFont text-xl text-white pb-4 text-center w-full`}>{subTitle}</h3>
+                    <h3 className={`text-xl text-white pb-4 text-center w-full`}>{subTitle}</h3>
                 :
                     <></>
             }
