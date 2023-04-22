efd521a2e2382b655a697b081e4e29635547053e
HenryLima07 <00033721@uca.edu.sv>
2023-04-22 09:54:37 -0600

    :lipstick: add style to header on hover

--- KFC/src/Components/Shared/Header/Menu/LinkElement/LinkElement.component.jsx
+++ KFC/src/Components/Shared/Header/Menu/LinkElement/LinkElement.component.jsx
@@ -6,7 +6,7 @@ import ext from "/img/art08.svg"
 
 const LinkElement = ({ children, to})=>{
     return(
-        <Link to={to} className=" min-w-[15%] flex justify-center uppercase hover:bg-[url('/img/art08.svg')] bg-contain bg-center bg-no-repeat">
+        <Link to={to} className=" min-w-[15%] flex justify-center uppercase hover:text-kfc-red-buttons hover:bg-[url('/img/art08.svg')] bg-contain bg-center bg-no-repeat">
             <li className=" mx-4 my-8">{ children }</li>
         </Link>
     )
